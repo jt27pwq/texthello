5bc9e4945dd7c44ed32e1c984b59e5730a1d0132
jt27pwq <1203262207@qq.com>
2018-01-08 15:07:00 +0800

    sd

---
+++ texthello/src/texttext/text.java
@@ -0,0 +1,12 @@
+package texttext;
+
+public class text {
+	public static void main(String[] args) {
+		System.out.println("a");
+		System.out.println("a");
+		System.out.println("b");
+		System.out.println("c");
+		
+
+	}
+}
