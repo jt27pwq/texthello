4e4b1d0003a99d22c53cc60203d65c18ac9b64a7
jt27pwq <1203262207@qq.com>
2018-01-08 15:27:10 +0800

    d

--- texthello-master/src/texttext/text.java
+++ texthello-master/src/texttext/text.java
@@ -3,20 +3,14 @@ package texttext;
 public class text {
 	public static void main(String[] args) {
 		System.out.println("a");
+
 		System.out.println("a");
-		System.out.println("b");
 		
 		System.out.println("a");
+		
 		System.out.println("a");
-		System.out.println("b");
-		
-		
-		
-		System.out.println("c");
-		
-		System.out.println("c");
-		
-		
+
+
 
 	}
 }
